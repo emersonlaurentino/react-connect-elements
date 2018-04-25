b0f115e8c0b44ec272b4e96ed259bdb621521c16
Emerson Laurentino <emersonlaurentino@hotmail.com>
2018-04-25 18:03:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# React Connect Elements
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Portal from './portal';
+
+const ConnectElements = () => (
+  <Portal>
+    <svg id="svg1" width="0" height="0" >
+      <path
+        id="path1"
+        d="M0 0"
+        stroke="#000"
+        fill="none"
+        stroke-width="12px"
+      />
+    </svg>
+  </Portal>
+);
+
+export default ConnectElements;
